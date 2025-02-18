f364ecd8dceb2133cfc4fb987c0bb2eddea34005
Tony Keiser <keisertony@gmail.com>
2025-02-18 14:05:53 -0700

    added page links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,22 @@
+import Link from 'next/link'
+
 export default function HomePage() {
-  return <div>Home Page</div>
+  return (
+    <div>
+      <nav>
+        <ul className="flex items-center gap-4">
+          <li>
+            <Link href="/performance">Performance</Link>
+          </li>
+          <li>
+            <Link href="/reliability">Reliability</Link>
+          </li>
+          <li>
+            <Link href="/scale">Scale</Link>
+          </li>
+        </ul>
+      </nav>
+      Home Page
+    </div>
+  )
 }
